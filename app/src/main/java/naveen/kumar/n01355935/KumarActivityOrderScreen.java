31180ecd5464a02e52fa34434f4500ac89b840e6
naveenkumar <naveenbti002@gmail.com>
2021-02-14 08:29:37 +0530

    Added Payment screen and implemented functions to check size, crust and toppings and selected

--- app/src/main/java/naveen/kumar/n01355935/KumarActivityOrderScreen.java
+++ app/src/main/java/naveen/kumar/n01355935/KumarActivityOrderScreen.java
@@ -4,11 +4,103 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
 public class KumarActivityOrderScreen extends AppCompatActivity {
+
+
+
+    public void sizeGroupCheck(View view) {
+        String sizeSelected=" ", crustSelected=" ";
+        ArrayList<String> toppingList = new ArrayList<String>();
+        RadioGroup sizeGroup = (RadioGroup) findViewById(R.id.naveenSizeGroup);
+        RadioGroup crustGroup = (RadioGroup) findViewById(R.id.naveenCrustGroup);
+
+        int sizeID = sizeGroup.getCheckedRadioButtonId();
+        int crustID = crustGroup.getCheckedRadioButtonId();
+
+        CheckBox capsicum = (CheckBox) findViewById(R.id.naveenCapsicumCheck);
+        CheckBox gPeppers = (CheckBox) findViewById(R.id.naveenGPeppersCheck);
+        CheckBox onions = (CheckBox) findViewById(R.id.naveenOnionCheck);
+        CheckBox pepperoni = (CheckBox) findViewById(R.id.naveenPepperoniCheck);
+        CheckBox mushroom = (CheckBox) findViewById(R.id.naveenMushroomCheck);
+        CheckBox bOlives = (CheckBox) findViewById(R.id.naveenOlivesCheck);
+        CheckBox spinach = (CheckBox) findViewById(R.id.naveenSpinachCheck);
+
+        if (sizeID == -1) {
+            Snackbar.make(view, "Please select size", Snackbar.LENGTH_LONG).show();
+        }else{
+            RadioButton sizeButton = findViewById(sizeID);
+            sizeSelected = sizeButton.getText().toString();
+            Log.i("size: ",sizeSelected);
+        }
+        if (crustID == -1) {
+            Snackbar.make(view, "Please select crust", Snackbar.LENGTH_LONG).show();
+        }else {
+            RadioButton crustButton = findViewById(crustID);
+            crustSelected = crustButton.getText().toString();
+            Log.i("crust:",crustSelected);
+        }
+
+        if(capsicum.isChecked()) {
+                        toppingList.add(capsicum.getText().toString());
+        }if(onions.isChecked()){
+            toppingList.add(onions.getText().toString());
+        }if(gPeppers.isChecked()){
+            toppingList.add(gPeppers.getText().toString());
+        } if(pepperoni.isChecked()){
+            toppingList.add(pepperoni.getText().toString());
+        }if(mushroom.isChecked()){
+            toppingList.add(mushroom.getText().toString());
+        } if(bOlives.isChecked()){
+            toppingList.add(bOlives.getText().toString());
+        } if(spinach.isChecked()){
+            toppingList.add(spinach.getText().toString());
+        }
+        else {
+                Log.i("Toppings:", "not selected");
+                Snackbar.make(view, "Please select toppings", Snackbar.LENGTH_LONG).show();
+            }
+
+        if ((capsicum.isChecked() || gPeppers.isChecked() || onions.isChecked() || pepperoni.isChecked() || mushroom.isChecked() || bOlives.isChecked() || spinach.isChecked()) && sizeID != -1 && crustID != -1) {
+            Snackbar.make(view, "Condition Satisfied", Snackbar.LENGTH_LONG).show();
+            Log.i("Size:",sizeSelected);
+            Log.i("crust: ",crustSelected);
+            Log.i("topping:",toppingList.get(0));
+
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/naveen/kumar/n01355935/KumarActivityPaymentScreen.java
@@ -0,0 +1,14 @@
+package naveen.kumar.n01355935;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KumarActivityPaymentScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kumar_payment_screen);
+    }
+}
