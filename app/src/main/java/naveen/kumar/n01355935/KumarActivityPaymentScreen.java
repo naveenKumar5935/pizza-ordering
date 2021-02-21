e90e858d65bc7861e01b595edc5c26fc3b3cdee7
naveenkumar <naveenbti002@gmail.com>
2021-02-21 23:49:44 +0530

    Applied conditions to check the fields with proper inputs and extracted string resources

--- app/src/main/java/naveen/kumar/n01355935/KumarActivityPaymentScreen.java
+++ app/src/main/java/naveen/kumar/n01355935/KumarActivityPaymentScreen.java
@@ -4,14 +4,128 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class KumarActivityPaymentScreen extends AppCompatActivity {
+
+
+    public void orderClick(View view){
+
+        EditText name = findViewById(R.id.naveenYourName);
+        EditText email = findViewById(R.id.naveenEmail);
+        EditText phoneNo = findViewById(R.id.naveenPhoneNo);
+        EditText address = findViewById(R.id.naveenAddress);
+        EditText postal = findViewById(R.id.naveenPosalCode);
+        EditText city = findViewById(R.id.naveenCity);
+        EditText creditNo = findViewById(R.id.naveenCreditNo);
+        EditText expiryDate = findViewById(R.id.naveenExpiryDate);
+        EditText cvv = findViewById(R.id.naveenCVV);
+
+        Boolean allConditionCheck=true;
+
+        char[] numbers = "0123456789".toCharArray();
+
+        for(char number: numbers){
+            if(name.getText().toString().contains(String.valueOf(number))){
+                name.setError(getString(R.string.name_alpha_error));
+                allConditionCheck=false;
+            }
+            if(city.getText().toString().contains(String.valueOf(number))){
+                city.setError(getString(R.string.city_alpha_error));
+                allConditionCheck=false;
+            }
+        }
+        if(name.length()<3){
+            name.setError(getString(R.string.name_length_error));
+            allConditionCheck=false;
+        }
+
+        if(phoneNo.length()<10){
+            phoneNo.setError(getString(R.string.phoneNo_length_error));
+            allConditionCheck=false;
+        }
+        if(postal.length()<6){
+            postal.setError(getString(R.string.postal_length_error));
+            allConditionCheck=false;
+        }
+        if(creditNo.length()<16){
+            creditNo.setError(getString(R.string.creditNo_length_error));
+            allConditionCheck=false;
+        }
+        if(expiryDate.length()<4){
+            expiryDate.setError(getString(R.string.expirydate_length_error));
+            allConditionCheck=false;
+        }
+        if(cvv.length()<3){
+            cvv.setError(getString(R.string.cvv_length_error));
+            allConditionCheck=false;
+        }
+
+        if(name.length()==0){
+            name.setError(getString(R.string.enter_name_error));
+            allConditionCheck=false;
+        }
+        if(email.length()==0){
+            email.setError(getString(R.string.enter_email_error));
+            allConditionCheck=false;
+        }
+        if(phoneNo.length()==0){
+            phoneNo.setError(getString(R.string.enterphn_error));
+            allConditionCheck=false;
+        }
+        if(address.length()==0){
+            address.setError(getString(R.string.enter_address_error));
+            allConditionCheck=false;
+        }
+        if(postal.length()==0){
+            postal.setError(getString(R.string.enter_postal_error));
+            allConditionCheck=false;
+        }
+        if(city.length()==0){
+            city.setError(getString(R.string.enter_City_error));
+            allConditionCheck=false;
+        }
+        if(creditNo.length()==0){
+            creditNo.setError(getString(R.string.enter_credit_error));
+            allConditionCheck=false;
+        }
+        if(expiryDate.length()==0){
+            expiryDate.setError(getString(R.string.enter_expiry_error));
+            allConditionCheck=false;
+        }
+        if(cvv.length()==0){
+            cvv.setError(getString(R.string.enter_cvv_error));
+            allConditionCheck=false;
+        }
+
+        if(allConditionCheck){
+            Snackbar.make(view,"Congratulations",Snackbar.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +155,5 @@ public class KumarActivityPaymentScreen extends AppCompatActivity {
         ArrayAdapter pA = new ArrayAdapter(this,android.R.layout.simple_spinner_item,province);
         pA.setDropDownViewResource(R.layout.spinner_list);
         provinceSpinner.setAdapter(pA);
-
-
     }
 }
