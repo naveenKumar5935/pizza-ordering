5621aa4c0ab05abc42dd38f0054af85d6f94b298
naveenkumar <naveenbti002@gmail.com>
2021-02-21 20:56:21 +0530

    Given proper ids to payment screen

--- app/src/main/java/naveen/kumar/n01355935/KumarActivityPaymentScreen.java
+++ app/src/main/java/naveen/kumar/n01355935/KumarActivityPaymentScreen.java
@@ -17,24 +17,27 @@ public class KumarActivityPaymentScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kumar_payment_screen);
-      //  TextView t1 = findViewById(R.id.textView);
-      //  TextView t2 = findViewById(R.id.textView2);
-       // TextView t3 = findViewById(R.id.textView3);
-       // ListView listView = findViewById(R.id.listView);
+        TextView t1 = findViewById(R.id.naveenPaymentSizeDisplay);
+        TextView t2 = findViewById(R.id.naveenPaymentCrustDisplay);
+        TextView t3 = findViewById(R.id.naveenPaymentToppingsView);
 
         Intent intent = getIntent();
         String size = getIntent().getStringExtra("sizeSelected");
         String crust = getIntent().getStringExtra("crustSelected");
         ArrayList<String> list = getIntent().getStringArrayListExtra("toppingSelected");
-        //t1.setText(size);
-      //  t2.setText(crust);
-     //   t3.setText(list.get(1));
 
-       // ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
-        //listView.setAdapter(adapter);
+        String str = list.get(0);
+        for(int i=1;i<list.size();i++){
+            str = str + ","+list.get(i);
+        }
+
+        t1.setText(size);
+        t2.setText(crust);
+        t3.setText(str);
+
+        //Spinner Code for province
         String[] province = {"ON","QC","BC","PE","NS","NB","NL","MB","SK","AB","YT","NT","NU"};
         Spinner provinceSpinner = findViewById(R.id.naveenSpinner);
-
         ArrayAdapter pA = new ArrayAdapter(this,android.R.layout.simple_spinner_item,province);
         pA.setDropDownViewResource(R.layout.spinner_list);
         provinceSpinner.setAdapter(pA);
